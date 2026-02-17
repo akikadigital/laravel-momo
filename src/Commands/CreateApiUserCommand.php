18b7b03f1abf6411537e29e97e3caa098fc8474f
Timothy Karani <kartimothy@gmail.com>
2026-02-17 05:26:11 +0300

    feat: show the user more when creating api user via CLI

--- src/Commands/CreateApiUserCommand.php
+++ src/Commands/CreateApiUserCommand.php
@@ -4,15 +4,31 @@ namespace Akika\MoMo\Commands;
 
 use Akika\MoMo\Actions\CreateApiUserAction;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Config;
 
 class CreateApiUserCommand extends Command
 {
-    public $signature = 'momo:create-api-user';
+    public $signature = 'momo:create-api-user {-Y|no-confirmation}';
 
     public $description = 'Create an API User using the configured Reference Id in the .env file.';
 
     public function handle(): int
     {
+        $requiresConfirmation = ! $this->hasOption('no-confirmation');
+
+        $env = Config::string('momo.env');
+        $secondaryKey = Config::string("momo.{$env}.secondary_key");
+        $xReferenceId = Config::string("momo.{$env}.user_reference_id");
+
+        $this->line('Creating an API User:');
+        $this->line("env: {$env}");
+        $this->line("Secondary Key: {$secondaryKey}");
+        $this->line("User Reference ID: {$xReferenceId}");
+
+        if ($requiresConfirmation && ! $this->confirm('Proceed?')) {
+            return self::SUCCESS;
+        }
+
         (new CreateApiUserAction)();
 
         return self::SUCCESS;
